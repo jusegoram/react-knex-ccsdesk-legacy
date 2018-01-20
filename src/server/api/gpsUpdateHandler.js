da0206a53c42ac47c81ca162f67194b2eb6c0246
Sebastian Gomez <jseb16@gmail.com>
2018-01-19 22:42:46 -0600

    remove loggign

--- src/server/api/gpsUpdateHandler.js
+++ src/server/api/gpsUpdateHandler.js
@@ -2,7 +2,6 @@
 import { knex } from '../../database'
 
 export default async (req, res) => {
-  console.log('gps request', req.body)
   if (req.body.secret !== 'fd97c1438493e83cebf651ac6779250dd78b64c424b14fea548dc24c7e797f7d') return res.sendStatus(401)
 
   const { username, latitude, longitude, timestamp } = req.body
